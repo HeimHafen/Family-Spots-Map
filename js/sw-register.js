81f88bbecac022f3c05890a048fd42f19c0feff2
FamilySpots-DE/DK <markus-eiben@gmx.de>
2025-11-13 09:07:32 +0100

    sw-register.js

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,15 +1,7 @@
-// Family Spots Map – Service Worker Registration (stable)
-(function () {
-  if (!("serviceWorker" in navigator)) return;
-
-  window.addEventListener("load", function () {
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("service-worker.js")
-      .then(function (reg) {
-        console.info("Service Worker registriert:", reg.scope);
-      })
-      .catch(function (err) {
-        console.warn("Service Worker Registrierung fehlgeschlagen:", err);
-      });
+      .catch((err) => console.error("Service Worker registration failed", err));
   });
-})();
+}
