37d673a3759ed951873726ec36eeca54ad7086e5
FamilySpots-DE/DK <markus-eiben@gmx.de>
2025-11-15 10:05:18 +0100

    Bump CACHE_NAME to version 42
    Updated cache version number for service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "family-spots-map-v41"; // <- Version hochgesetzt
+const CACHE_NAME = "family-spots-map-v42"; // <- Version hochgesetzt
 const OFFLINE_URL = "offline.html";
 
 const ASSETS = [
