c9bf5bd03bf934ea16c4c70e19edc155c28c98bc
FamilySpots-DE/DK <markus-eiben@gmx.de>
2025-11-18 20:05:40 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // Version des Caches – bei Änderungen an Assets INKREMENTIEREN
-const CACHE_NAME = "family-spots-map-24";
+const CACHE_NAME = "family-spots-map-25";
 const OFFLINE_URL = "offline.html";
 
 const ASSETS = [
