cc894501e0b1a6d826ef57397a1baec57758e7e1
FamilySpots-DE/DK <markus-eiben@gmx.de>
2025-12-13 16:29:24 +0100

    dom.js

--- js/utils/dom.js
+++ js/utils/dom.js
@@ -1,7 +1,24 @@
 // js/utils/dom.js
+// -----------------------------------------
+// Mini-Utils für DOM-Zugriff
+// -----------------------------------------
+
+/**
+ * Kurzform für querySelector.
+ * @param {string} sel - CSS-Selektor
+ * @param {ParentNode} [root=document]
+ * @returns {Element|null}
+ */
 export function qs(sel, root = document) {
   return root.querySelector(sel);
 }
+
+/**
+ * Kurzform für querySelectorAll als Array.
+ * @param {string} sel - CSS-Selektor
+ * @param {ParentNode} [root=document]
+ * @returns {Element[]}
+ */
 export function qsa(sel, root = document) {
   return Array.from(root.querySelectorAll(sel));
 }
