cc0b4c69d8f6ae3e8e863f58298c0956f8ffc655
FamilySpots-DE/DK <markus-eiben@gmx.de>
2025-12-22 19:07:35 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_VERSION = "39"; // <— IMPORTANT: bump on any release
+const CACHE_VERSION = "40"; // <— IMPORTANT: bump on any release
 const CACHE_NAME = `family-spots-map-${CACHE_VERSION}`;
 const OFFLINE_URL = "offline.html";
 
